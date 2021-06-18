cdefc782cd75fa72b18e6c34e05f47ae2cb14473
Mellier Clement <clement.mellier@insa-cvl.fr>
2021-06-18 16:14:36 +0200

    import texture model girl

--- 3D_game/src/sandbox.py
+++ 3D_game/src/sandbox.py
@@ -1,4 +1,5 @@
 from ursina import *
+from os import path
 
 app = Ursina()
 window.title = 'My fucking game'                # The window title
@@ -9,8 +10,26 @@ window.exit_button.visible = False
 # Show the FPS (Frames per second) counter
 window.fps_counter.enabled = True
 
+root_folder = Path(__file__).parent.parent
+asset_folder = root_folder / 'asset/'
+texture_folder = asset_folder / 'texture/'
 
-player = Entity(model='cube', color=color.blue, scale_y=2)
+girl = load_model("girl_OBJ.obj", path=asset_folder, file_types=(
+    '.bam', '.ursinamesh', '.obj', '.glb', '.gltf', '.blend'))
+
+my_texture = load_texture("bot color.jgp", path=texture_folder)
+
+player = Entity(model=girl, position=0,
+                scale=10, double_sided=True)  # , texture='white_cube')
+
+ground = Entity(model=Terrain('heightmap_1'),
+                scale=(20, 5, 20), texture='heightmap_1')
+
+EditorCamera()
+camera.orthographic = True
+
+pivot = Entity()
+DirectionalLight(parent=pivot, x=0, y=.15, z=0, shadows=True)
 
 
 def update():   # update gets automatically called.
@@ -21,6 +40,5 @@ def update():   # update gets automatically called.
 
     # et ça tourne tourne toune c'est ta façon  d'aimer.
     player.rotation_y = player.rotation_y + time.dt*100
-
 
 app.run()   # opens a window and starts the game.
