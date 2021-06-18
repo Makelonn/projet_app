f239bc0ca1983f985bad3c802f6e3fed9bf1ee5e
Makelonn <maelie.lebaron@gmail.com>
2021-06-18 19:44:10 +0200

    worked on relative mov and collision

--- 3D_game/3D_advancement.md
+++ 3D_game/3D_advancement.md
@@ -3,4 +3,5 @@
 ## Obstacles
 
 - Documentation gives a list of model but some of them seems to not work 
-- Text display is easy
+- Text display is easy, auto \n
+- Rotation coordonate and parent is a bit hard to understand
--- 3D_game/src/testfile_ground.py
+++ 3D_game/src/testfile_ground.py
@@ -1,9 +1,15 @@
-from ursina import * 
+from ursina import *   
 
 app = Ursina()
-
-player = Entity(model='sphere', color=color.orange, texture='brick')
-title = Text(text='Here is a game !')
+window.title = 'Baj'
+g_path = Path(__file__).parent.parent / "asset" / "girl"
+g_model = load_model('girl OBJ.obj', path=g_path)
+g_text = load_texture('girl OBJ', path=g_path)
+player = Entity(model=g_model, texture=g_text)
+title = Text(text='1234 56 Here is a game !123456789123456789', origin=(-.10,.5), wordwrap=1)
+tv = Entity(model='sphere', texture="../asset/video.mp4", parent=player, origin_x=-2, origin_y=-2, origin_z=+2)
+ground = Entity(model=Terrain('heightmap_1'), scale=(20,5,20), texture='heightmap_1')
+EditorCamera()
 
 def update():   # update gets automatically called.
     player.x += held_keys['d'] * .1
