a2be0673f82b39771dad9bfc3e9948c1d51e39ec
Mellier Clement <clement.mellier@insa-cvl.fr>
2021-06-19 11:55:17 +0200

    [3D game] try station spatiale

---
+++ 3D_game/src/ground.py
@@ -0,0 +1,26 @@
+from ursina import *
+
+app = Ursina()
+window.title = 'My fucking game'                # The window title
+window.borderless = False               # Show a border
+window.fullscreen = False               # Do not go Fullscreen
+# Do not show the in-game red X that loses the window
+window.exit_button.visible = False
+# Show the FPS (Frames per second) counter
+window.fps_counter.enabled = True
+
+root_folder = Path(__file__).parent.parent
+asset_folder = root_folder / 'asset/'
+texture_folder = asset_folder / 'texture/'
+
+station_folder = asset_folder / 'station_spatiale/'
+
+EditorCamera()
+camera.orthographic = True
+
+print("\n\n station folder :", station_folder)
+
+level = load_blender_scene('Space Station Scene',
+                           path=station_folder)
+
+app.run()   # opens a window and starts the game.
