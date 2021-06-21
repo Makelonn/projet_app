79f1fc4a9ff468286b80ea2506e7b165ff9073e8
Makelonn <maelie.lebaron@gmail.com>
2021-06-21 15:42:53 +0200

    panda3d : loading env

--- 3D_panda/main.py
+++ 3D_panda/main.py
@@ -1,9 +1,20 @@
-import panda3d as pa
 from direct.showbase.ShowBase import ShowBase
+from direct.actor.Actor import Actor
+from panda3d.core import WindowProperties
 
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+        properties = WindowProperties()
+        properties.setTitle('Wow, a game !')
+        properties.setSize(1024,576)
+        self.win.requestProperties(properties)
+        # Camera not controlled by the mouse
+        self.disable_mouse()
+        # .egg is human readable format -> converted to .bam by panda3d
+        self.environment = loader.loadModel("Sample_model/Env/environment")
+        self.environment.reparentTo(render)
+
 
 app = Game()
 app.run()
