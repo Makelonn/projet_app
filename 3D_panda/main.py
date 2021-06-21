a7f01f489ad9ee5da8b91cb7555f4d222d43029b
Makelonn <maelie.lebaron@gmail.com>
2021-06-21 15:07:01 +0200

    panda3d sample

--- 3D_panda/main.py
+++ 3D_panda/main.py
@@ -1,9 +1,9 @@
 import panda3d as pa
 from direct.showbase.ShowBase import ShowBase
 
-class Main(ShowBase):
+class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
-app = Main()
+app = Game()
 app.run()
